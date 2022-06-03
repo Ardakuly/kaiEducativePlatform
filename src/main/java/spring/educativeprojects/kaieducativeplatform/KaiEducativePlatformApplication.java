3ce38dec84cab9535f4b8df88444ed282e1b6970
Alisher Anuarbekov <ardakuly@Alishers-MacBook-Pro.local>
2022-06-04 01:26:21 +0600

    Initial commitment

---
+++ src/main/java/spring/educativeprojects/kaieducativeplatform/KaiEducativePlatformApplication.java
@@ -0,0 +1,13 @@
+package spring.educativeprojects.kaieducativeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiEducativePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiEducativePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/educativeprojects/kaieducativeplatform/KaiEducativePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.educativeprojects.kaieducativeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiEducativePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
